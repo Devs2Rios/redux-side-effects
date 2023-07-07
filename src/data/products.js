47dd043c79dec618c1e51b63909c518267e98f60
eduairet <hola@eduairet.com>
2023-07-06 22:24:08 -0600

    Dummy data

---
+++ src/data/products.js
@@ -0,0 +1,16 @@
+const products = [
+    {
+        id: 'product-001',
+        title: 'Fish',
+        price: 6,
+        description: 'Fresh fish from the sea'
+    },
+    {
+        id: 'product-002',
+        title: 'Soda',
+        price: 3,
+        description: 'Refreshing soda'
+    },
+];
+
+export default products;
