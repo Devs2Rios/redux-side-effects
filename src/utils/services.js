7526e2f8f8f82ec5a72f3eeb1d71f9feff444e63
eduairet <hola@eduairet.com>
2023-07-18 23:10:05 -0600

    Deploy commit

--- src/utils/services.js
+++ src/utils/services.js
@@ -1,21 +1,21 @@
 import { apiUrl } from './config';
 
 export const getProducts = async () => {
-    const response = await fetch(`${apiUrl}/products.json`)
+    const response = await fetch(`${apiUrl}products.json`)
         .then(data => data.json())
         .catch((err) => console.error(err));
     return response;
 }
 
 export const getCart = async () => {
-    const response = await fetch(`${apiUrl}/cart.json`)
+    const response = await fetch(`${apiUrl}cart.json`)
         .then(data => data.json())
         .catch(err => console.error(err));
     return response
 }
 
 export const replaceCart = async (items) => {
-    const response = await fetch(`${apiUrl}/cart.json`,
+    const response = await fetch(`${apiUrl}cart.json`,
         {
             method: 'PUT',
             body: JSON.stringify({ items }),
